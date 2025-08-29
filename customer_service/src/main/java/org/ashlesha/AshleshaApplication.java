d1cc7c99634885c0f9def7e81f7cda082a022588
Tilak Basnet <164093341+basnet-tilak@users.noreply.github.com>
2025-08-29 10:38:49 +0200

    Initial commit

---
+++ customer_service/src/main/java/org/ashlesha/AshleshaApplication.java
@@ -0,0 +1,12 @@
+package org.ashlesha;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshleshaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AshleshaApplication.class,args);
+    }
+}
